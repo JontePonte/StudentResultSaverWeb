686a7540e4bea2af3d1f2ce67d2e9f31eabd1b69
JontePonte <john_nordstrand@hotmail.com>
2022-11-03 21:58:37 +0100

    Added base and home templates and got em working

--- website/main/views.py
+++ website/main/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
-def index(response):
-    return HttpResponse("<h1>StudentResultSaver!</h1>")
+def home(response):
+    return render(response, "main/home.html", {"login":False})
