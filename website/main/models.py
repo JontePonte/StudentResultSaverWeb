2e8944ee8c9f53c6ba17b413499131a74f263339
JontePonte <john_nordstrand@hotmail.com>
2022-11-03 22:24:28 +0100

    Started work on database structure

--- website/main/models.py
+++ website/main/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Group(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="group", null=True)
+    name = models.CharField(max_length=200)
+    description = models.CharField(max_length=500, default="My favorite class")
+
+    def __str__(self):
+        return str(self.name)
+    
+
+class Student(models.Model):
+    user = models.ForeignKey(Group, on_delete=models.CASCADE, related_name="student", null=True)
+    name = models.CharField(max_length=300)
+
+    def __str__(self):
+        return str(self.name)
